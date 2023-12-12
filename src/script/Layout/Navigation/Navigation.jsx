e8701a4ca769e14772b9136a14c8a71fee8d5ba2
Kent Erik <kent.erik.hole@outlook.com>
2023-12-12 19:18:18 +0100

    working on nav

--- src/script/Layout/Navigation/Navigation.jsx
+++ src/script/Layout/Navigation/Navigation.jsx
@@ -1,10 +1,19 @@
 // Navigation
     // Import 
+    // Logo 
+    import LogoImg from '../../../assets/Image/Logo/logo.svg'
 
 function Navigation () {
     return (
-        <>
-        </>
+        <nav>
+            <div>
+                <img src={LogoImg} alt="" />
+                <h1>
+                    Kent Erik Hole
+                </h1>
+            </div>
+
+        </nav>
     )
 
 
