8815b1cd34c762eed2e4618f332de89d6036b96c
Kent Erik <kent.erik.hole@outlook.com>
2023-12-12 18:12:11 +0100

    start on the Browser and the layout design

--- src/script/App.jsx
+++ src/script/App.jsx
@@ -1,7 +1,15 @@
 import '../css/style.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Layout/Layout'
+
+
 
 function App() {
-  return <></>;
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}> </Route>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App;
