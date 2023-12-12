a82478f69788ef0ddfed0d934f20ae91d596d87d
Kent Erik <kent.erik.hole@outlook.com>
2023-12-12 21:10:17 +0100

    adding App.jsx to this fil

--- src/script/App.jsx
+++ src/script/App.jsx
@@ -3,11 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './Layout/Layout'
 
 function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Layout />}> </Route>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}> 
+      
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
