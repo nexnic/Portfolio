1bdb1f46b4667089dd180a3ebebe6dbc2bc59c86
Kent Erik <kent.erik.hole@outlook.com>
2023-12-12 18:12:47 +0100

    start on the function and Layout

--- src/script/Layout/Layout.jsx
+++ src/script/Layout/Layout.jsx
@@ -1,2 +1,13 @@
 // Layout
 // Import 
+    // Navigation
+        import Navigation from './Navigation/Navigation'
+
+function Layout () {
+    return (
+        <>
+        <Navigation />
+        </>
+    )
+}
+export default Layout
