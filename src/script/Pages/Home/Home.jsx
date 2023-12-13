aeb0594835d293f0cbb7ef9cec9c812e99a2e218
Kent Erik <kent.erik.hole@outlook.com>
2023-12-13 19:39:04 +0100

    adding the Home Layout

---
+++ src/script/Pages/Home/Home.jsx
@@ -0,0 +1,25 @@
+/**
+ * @name Home
+ * @description This will be the Home page for this site
+ * @returns Main 
+ */
+
+
+
+function Home () {
+    return (
+        <main className="container">
+            <section id="About">
+                <h2>About</h2>
+            </section>
+            <section id="Experience">
+                <h2>Experience</h2>
+            </section>
+            <section id="Project">
+                <h2>Project</h2>
+            </section>
+        </main>
+    )
+}
+
+export default Home
